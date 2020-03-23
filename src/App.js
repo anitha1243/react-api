6a1ac4aa2134950fc2954dcb5a7ab12bb015fea8
anitha1243 <kurian.anithajose90@gmail.com>
2020-03-24 00:37:01 +1100

    Hello

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import HomePageLayout from './components/HomePageLayout.js'
+
+
+class App extends Component {
+	render(){
+		return(
+		<HomePageLayout />
+	)
+}
+}
+
+export default App;
---
+++ src/components/HomePageLayout.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react'
+import impactLogo from '../images/impactLogo.png'
+
+    export default class HomePageLayout extends Component {
+    render(){
+    return(<img src={impactLogo} alt="Impact Logo"/>)}
+    }
+	
