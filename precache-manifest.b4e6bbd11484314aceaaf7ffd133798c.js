13c265a47a6e7f229488313cab50cd81a2fa6824
anitha1243 <kurian.anithajose90@gmail.com>
2020-03-24 00:27:33 +1100

    Updates

---
+++ precache-manifest.b4e6bbd11484314aceaaf7ffd133798c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "137f92434d2de9f3ca3d23ebb24a0656",
+    "url": "/react-api/index.html"
+  },
+  {
+    "revision": "4b9c21b9fb5e6bdd3ec5",
+    "url": "/react-api/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f08abc6f89ee9b2d79c1",
+    "url": "/react-api/static/js/2.d4f83a03.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-api/static/js/2.d4f83a03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b9c21b9fb5e6bdd3ec5",
+    "url": "/react-api/static/js/main.6ace0986.chunk.js"
+  },
+  {
+    "revision": "b8bdd122ef61b3df53d1",
+    "url": "/react-api/static/js/runtime-main.3840df6a.js"
+  },
+  {
+    "revision": "78f128f5dc307f26b6ace87183b7d4c0",
+    "url": "/react-api/static/media/impactLogo.78f128f5.png"
+  }
+]);
